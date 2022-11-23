ec3cd3f4a2aeaab5ab299a57df29b327655adda2
Taylor Hurt <t>
2022-11-23 09:35:58 -0600

    hello world

--- main/django_project/settings.py
+++ main/django_project/settings.py
@@ -39,9 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
+    'django.contrib.sessions', 
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "pages.apps.PagesConfig",  # new
 ]
 
 MIDDLEWARE = [
