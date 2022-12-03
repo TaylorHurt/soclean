973b0d20f75516c872a748b2af5cdab53cab348b
Taylor Hurt <t>
2022-12-03 14:21:49 -0600

    12/3/2022.2

--- main/pages/tests.py
+++ main/pages/tests.py
@@ -10,31 +10,14 @@ class HomepageTests(SimpleTestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_url_available_by_name(self):
-        response = self.client.get(reverse("home"))
+        response = self.client.get(reverse("So Clean Amarillo Texas"))
         self.assertEqual(response.status_code, 200)
 
     def test_template_name_correct(self):  # new
-        response = self.client.get(reverse("home"))
+        response = self.client.get(reverse("So Clean Amarillo Texas"))
         self.assertTemplateUsed(response, "home.html")
 
     def test_template_content(self):  # new
-        response = self.client.get(reverse("home"))
-        self.assertContains(response, "<h1>Homepage</h1>")
-
-
-class AboutpageTests(SimpleTestCase):
-    def test_url_exists_at_correct_location(self):
-        response = self.client.get("/about/")
-        self.assertEqual(response.status_code, 200)
-
-    def test_url_available_by_name(self):
-        response = self.client.get(reverse("about"))
-        self.assertEqual(response.status_code, 200)
-
-    def test_template_name_correct(self):  # new
-        response = self.client.get(reverse("about"))
-        self.assertTemplateUsed(response, "about.html")
-
-    def test_template_content(self):  # new
-        response = self.client.get(reverse("about"))
-        self.assertContains(response, "<h1>About page</h1>")
+        response = self.client.get(reverse("So Clean Amarillo Texas"))
+        self.assertContains(
+            response, "<h1>Amarillo Texas Window Cleaning Services</h1>")
